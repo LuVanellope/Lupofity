fb2aa2c73385e89cfe18a31ed8ccb33914c409a3
Lu <luacosta.fc@gmail.com>
2018-06-15 22:34:38 -0500

    Adding keys

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,5 @@
 class Album < ApplicationRecord
   belongs_to :artist
   has_many :songs
+  add_foreign_key :artists_id
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,4 @@
 class Song < ApplicationRecord
   belongs_to :albums
+  add_foreing_key :album_id
 end
