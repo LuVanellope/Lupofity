8ce7d6e32aba253f38fb315ebd93b3daf5c84805
Lu <luacosta.fc@gmail.com>
2018-06-18 13:53:45 -0500

    adding foreing key to albums

--- db/migrate/20180618184953_create_albums.rb
+++ db/migrate/20180618184953_create_albums.rb
@@ -1,9 +1,10 @@
 class CreateAlbums < ActiveRecord::Migration[5.1]
   def change
     create_table :albums do |t|
-      t.string :name
-      t.string :image_url
-      t.string :released_at
+      t.references :artist, foreign_key: true
+      t.text :name
+      t.integer :number
+      t.string :preview_url
 
       t.timestamps
     end
